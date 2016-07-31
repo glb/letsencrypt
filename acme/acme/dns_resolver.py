d0933a7f367bc151a00abb32f16492297048b4ba
Wilfried Teiken <wteiken@google.com>
2016-07-31 19:14:08 -0400

    Experiment with resolver.

--- acme/acme/dns_resolver.py
+++ acme/acme/dns_resolver.py
@@ -25,4 +25,4 @@ def txt_records_for_name(name):
     except dns.exception.DNSException as error:
         logger.error("Error resolving %s: %s", name, str(error))
         return []
-    return [txt_rec for rdata in dns_response for txt_rec in rdata.strings]
+    return [txt_rec for rdata in dns_response]
